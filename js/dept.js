1b18a85f759cd4bacc1a35899d90061996045c9b
shubh1996 <shubhmehta1996@gmail.com>
2017-03-26 14:43:08 +0530

    material icons updated

--- js/dept.js
+++ js/dept.js
@@ -1,9 +1,8 @@
-<script type="text/javascript"> -->
-       $(document).ready(function(){
+$(document).ready(function(){
       $('.slider').slider();
     });
         
-</script>
+
 
 $(document).ready(function(){
 
