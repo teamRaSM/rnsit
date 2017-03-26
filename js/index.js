0e632f51b57dca0339a240084190433dac8d1505
sanair96 <sandeep_sashok@hotmail.com>
2017-03-26 21:27:59 +0530

    Sidenav added and fixed

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$( ".logo" ).fadeIn( 600, function(){
+  $(".logo").css("display","inline");
+      $( ".heading" ).fadeIn( 600, function() {
+        $(".heading").css("display","inline");
+      $( ".tag" ).fadeIn( 600, function() {
+        $(".tag").css("display","inline");
+      });
+  });
+
+  });
+  
+
+});
--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,50 @@
 $(document).ready(function(){
+$('.dropdown-button').dropdown({
+inDuration: 300,
+outDuration: 225,
+constrainWidth: false, // Does not change width of dropdown to that of the activator
+hover: true, // Activate on hover
+gutter: 0, // Spacing from edge
+belowOrigin: true, // Displays dropdown below the button
+alignment: 'left', // Displays dropdown with edge aligned to the left of button
+stopPropagation: false // Stops event propagation
+}
+);
 
-  $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
-  );
-    
+
+  $(".innerNav1").slideUp(300);
+            $(".innerNav").slideUp(300);
+      $(".dropNav").click(function(){
+      				if(sessionStorage.getItem('slide') == 0){
+      					$(".innerNav").slideUp(300);
+      					sessionStorage.setItem('slide',1);
+      				}
+      				else{
+      				sessionStorage.setItem('slide',0);
+      				$(".innerNav").slideDown("fast");
+      			}
+          });
+      $(".dropNav1").click(function(){
+          // alert('hi');
+              if(sessionStorage.getItem('slide1') == 0){
+                $(".innerNav1").slideUp(300);
+                sessionStorage.setItem('slide1',1);
+              }
+              else{
+              sessionStorage.setItem('slide1',0);
+              $(".innerNav1").slideDown("fast");
+
+}
+      });
+
+
+
+
+
+$(".button-collapse").sideNav({
+menuWidth:300,
+edge:'left',
+closeOnClick:false,
+draggable:true});
+
 });
